b424bcc7549058de2238b3b0e376d5a4fcd8331f
Sergey Filippovskikh <me@filippovskikh.ru>
2025-01-17 19:25:08 +0300

    Спринт 11. Рейтинг Фильмов. Исправление ошибок стиля в ГитХабе

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,8 @@
 package ru.yandex.practicum.filmorate;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 
 @SpringBootTest
 class FilmorateApplicationTests {
-
-
 
 }
