9faafaf4256ba799fad3c0a6fc34b388ad34d3dc
Sergey Filippovskikh <me@filippovskikh.ru>
2025-02-17 14:58:33 +0300

    Спринт 12. Рейтинг Фильмов. Перевод проекта на хранение данных в базе данных H2. Корректировка стиля по GitHub.

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,32 +1,8 @@
 package ru.yandex.practicum.filmorate;
 
-import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.UserDbStorage;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
+@SpringBootTest
+public class FilmorateApplicationTests {
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-@JdbcTest
-@AutoConfigureTestDatabase
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
-class FilmorateApplicationTests {
-    private final UserDbStorage userStorage;
-
-    @Test
-    public void testFindUserById() {
-
-        Optional<User> userOptional = Optional.ofNullable(userStorage.getById(1));
-
-        assertThat(userOptional)
-                .isPresent()
-                .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
-                );
-    }
 }
